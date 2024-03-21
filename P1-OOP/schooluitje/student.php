8ffae593cf46a06c8508e698a76d0fb2500eef64
RohanDayal1 <9019234@student.zadkine.nl>
2024-03-21 16:03:44 +0100

    fix

--- P1-OOP/schooluitje/student.php
+++ P1-OOP/schooluitje/student.php
@@ -6,13 +6,20 @@ $schoolTripList = new SchoolTripList();
 $schoolTripList->addStudent(new Student('Koningstein', '', '', ''));
 $schoolTripList->addStudent(new Student('', 'Alex', 'sod2a', 'Nee'));
 $schoolTripList->addStudent(new Student('', 'Gerard', 'sod2a', 'Ja'));
+$schoolTripList->addStudent(new Student('', 'Bob', 'sod2a', 'Ja'));
+$schoolTripList->addStudent(new Student('', 'Marley', 'sod2a', 'Ja'));
 $schoolTripList->addStudent(new Student('', 'Liam', 'sod2b', 'Ja'));
 $schoolTripList->addStudent(new Student('Brugge', '', '', ''));
 $schoolTripList->addStudent(new Student('', 'Yadira', 'sod2b', 'Ja'));
 $schoolTripList->addStudent(new Student('', 'Omar', 'sod2a', 'Nee'));
+$schoolTripList->addStudent(new Student('', 'Jan', 'sod2a', 'Ja'));
+$schoolTripList->addStudent(new Student('', 'Bernard', 'sod2a', 'Ja'));
 $schoolTripList->addStudent(new Student('', 'Isabella', 'sod2b', 'Ja'));
 $schoolTripList->addStudent(new Student('Drimmelen', '', '', ''));
 $schoolTripList->addStudent(new Student('', 'Lucas', 'sod2b', 'Nee'));
+$schoolTripList->addStudent(new Student('', 'Pagani', 'sod2a', 'Ja'));
+$schoolTripList->addStudent(new Student('', 'Horacio', 'sod2a', 'Ja'));
+$schoolTripList->addStudent(new Student('', 'Lee', 'sod2a', 'Ja'));
 $schoolTripList->addStudent(new Student('', 'Chardy', 'sod2a', 'Ja'));
 
 
