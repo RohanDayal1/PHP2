f8bac52ec459d71f58c626a3360fcb3f2e3a0a2c
RohanDayal1 <9019234@student.zadkine.nl>
2024-04-08 11:02:42 +0200

    Dobbelspel

---
+++ P1-OOP/dobbelspel/Dice.php
@@ -0,0 +1,18 @@
+<?php
+// auteur: RohanD
+
+class Dice {
+    const NUMBER_OF_SIDES = 6;
+    private $faceValue;
+
+    public function throwDice() {
+        $this->faceValue = rand(1, self::NUMBER_OF_SIDES);
+    }
+
+    public function getFaceValue() {
+        return $this->faceValue;
+    }
+
+}
+
+?>
